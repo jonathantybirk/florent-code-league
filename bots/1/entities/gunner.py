b0ab17c84fbca1ec6f6480df2f75b080e868f39a
Lucas <lucas191202@gmail.com>
2026-07-28 15:26:17 +0200

    improve strat1
    - when nothing to do for attackers
    - conveyor fix

--- bots/1/entities/gunner.py
+++ bots/1/entities/gunner.py
@@ -7,4 +7,13 @@ from fcode import Controller
 
 class GunnerMixin:
     def run_gunner(self, ct: Controller) -> None:
-        pass
+        target = ct.get_gunner_target()
+        if target is None:
+            return
+        entity_id = ct.get_tile_builder_bot_id(target)
+        if entity_id is None:
+            entity_id = ct.get_tile_building_id(target)
+        if entity_id is None or ct.get_team(entity_id) == ct.get_team():
+            return
+        if ct.can_fire(target):
+            ct.fire(target)
